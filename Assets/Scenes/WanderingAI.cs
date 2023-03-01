30aa4b5c233e8551daf3fe0e32d55ff6200b9621
Virtya <nik.zheltikov.26@gmail.com>
2023-03-01 14:02:26 +0300

    firstCom
    01.03.2023

---
+++ Assets/Scenes/WanderingAI.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{ 
+    // Как и в сценарии SceneController
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+    private bool _alive;
+
+    void Start()
+    {
+        _alive = true;
+    }
+
+    void Update()
+    {
+        if (_alive)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.SphereCast(ray, 0.75f, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>())  // Игрок распознается таким же путем, что и мишень в сценарии RayShooter
+                {
+                    if (_fireball == null)  // Логика с пустым игровым объектом
+                    {
+                        _fireball = Instantiate(fireballPrefab) as GameObject;
+
+                        // Помещение огненного шара перед врагом и нацеливания в направлении его движения
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+                else if (hit.distance < obstacleRange)
+                {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
+            }
+        }
+    }
+
+    public void SetAlive(bool alive)
+    {
+        _alive = alive;
+    }
+}
